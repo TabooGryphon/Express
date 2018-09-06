d8c163adeb8b2d859430d78211c5978695f9a853
Ryan <ryanasmith@ksu.edu>
2018-09-06 16:45:52 -0500

    Added cool page
    Completed

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET cool page*/
+router.get('/cool', function(req, res, next){
+  res.send('Your\'re so cool');
+});
+
 module.exports = router;
