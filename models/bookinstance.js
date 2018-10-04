5b6c574482b4a33484b1ba414113947287fc2a7e
Ryan <ryanasmith@ksu.edu>
2018-10-04 10:36:07 -0500

    Fixed Date Format
    Made the date format look better

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -14,6 +14,13 @@ var BookInstanceSchema = new Schema(
 
 // Virtual for bookinstance's URL
 BookInstanceSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/bookinstance/' + this._id;
+});
+
+// Virtual for bookinstance's URL
+BookInstanceSchema
 .virtual('due_back_formatted')
 .get(function () {
   return moment(this.due_back).format('MMMM Do, YYYY');
